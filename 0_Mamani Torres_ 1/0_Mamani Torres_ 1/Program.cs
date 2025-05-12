6021138f443614aa324e1a5cf3995f79f6252fd3
MNinamam <matiasnico2112@gmail.com>
2025-05-12 09:19:50 -0300

    Add files via upload

---
+++ 0_Mamani Torres_ 1/0_Mamani Torres_ 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Mamani_Torres__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+
+            palabra = ("Hola mundo!");
+            Console.WriteLine(palabra);
+            Console.ReadKey();
+        }
+    }
+}
