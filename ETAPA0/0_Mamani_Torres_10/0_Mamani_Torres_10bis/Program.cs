1917c0eb33afaaff1c52b91419e70bab95519703
MNinamam <matiasnico2112@gmail.com>
2025-05-12 10:21:19 -0300

    Add files via upload

---
+++ ETAPA0/0_Mamani_Torres_10/0_Mamani_Torres_10bis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Mamani_Torres_10bis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grados;
+            Console.WriteLine(" Ingrese una cantidad de grados ");
+            grados = int.Parse(Console.ReadLine());
+            Double faren = (grados * 1.8) + 32;
+            Console.WriteLine(" De grados a farenheit es " + faren);
+            Double kelvin = grados + 273.5;
+            Console.WriteLine(" De gradso a kelvin es " + kelvin);
+            Console.ReadKey();
+        }
+    }
+}
