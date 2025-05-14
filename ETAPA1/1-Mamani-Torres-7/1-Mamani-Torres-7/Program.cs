4d5ac86565b5eb6a2952ff3c14c8e3fb65ee2c3f
MNinamam <matiasnico2112@gmail.com>
2025-05-14 18:31:15 -0300

    Add files via upload

---
+++ ETAPA1/1-Mamani-Torres-7/1-Mamani-Torres-7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Mamani_Torres_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+
+            Console.WriteLine(" Ingrese una palabra ");
+            palabra = Console.ReadLine();
+
+            Console.WriteLine( palabra + " " + palabra  + " " + palabra + " " + palabra + " " + palabra + " " + palabra + " " + palabra + " " + palabra + " " + palabra + " " + palabra);
+
+           
+
+        }
+    }
+}
+
---
+++ ETAPA1/1-Mamani_Torres-6/1-Mamani_Torres-6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Mamani_Torres_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradosC;
+
+            Console.WriteLine(" Ingresar cantidad de grados ");
+            gradosC = int.Parse(Console.ReadLine());
+
+            if (gradosC <= 0)
+            {
+                Console.WriteLine(" Se congela");
+            }
+            else if (gradosC >= 100)
+            {
+                Console.WriteLine(" Hierve ");
+            }
+            else if (gradosC >= 1 && gradosC < 100)
+            {
+                Console.WriteLine(" Esta en estado liquido ");
+            }
+
+
+        }
+    }
+}
