397c83b452d712a60985d2304a2b66ef5afeebd4
MNinamam <matiasnico2112@gmail.com>
2025-05-15 08:52:57 -0300

    Add files via upload

---
+++ ETAPA1/1-Mamani-Torres-7/1-Mamani-Torres-7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Mamani_Torres_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+
+            Console.WriteLine(" Ingrese una palabra ");
+            palabra = Console.ReadLine();
+
+            for ( int contador = 0; contador < 10; contador++)
+            {
+                Console.WriteLine(palabra);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
