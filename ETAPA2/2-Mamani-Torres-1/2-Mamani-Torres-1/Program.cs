98e13eebb36bc169fca4c159146a401fe03c95a5
MNinamam <matiasnico2112@gmail.com>
2025-05-26 09:33:22 -0300

    Add files via upload

---
+++ ETAPA2/2-Mamani-Torres-1/2-Mamani-Torres-1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _2_Mamani_Torres_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" De cuantas vueltas es la carrera ?");
+            int vueltas = int.Parse(Console.ReadLine());
+            int[] tievueltas = new int[vueltas];
+
+            for (int i = 1; i <= vueltas; i++)
+            {
+                Console.WriteLine(" Cual fue le tiempo de la vuelta N: " + i + " ? ");
+                tievueltas[i-1] = int.Parse(Console.ReadLine());
+            }
+
+            double tiempoTotal = 0;
+            int mejortiempo = tievueltas[0];
+            int mejorvuelta = 1;
+
+            for (int y = 0; y < vueltas; y++)
+            {
+                tiempoTotal += tievueltas[y];
+
+                if (tievueltas[y] < mejortiempo)
+                {
+                    mejortiempo = tievueltas[y];
+                    mejorvuelta = y + 1;
+                }
+            }
+            double promedio = tiempoTotal / vueltas;
+
+            Console.WriteLine("Tiempo total de carrera: " + tiempoTotal + " segundos ");
+            Console.WriteLine("Promedio por vuelta: " + promedio + " segundos");
+            Console.WriteLine("Mejor vuelta: Vuelta " + mejorvuelta + " con " + mejortiempo + " segundos");
+
+            Console.ReadKey();
+        }     
+    }
+}
---
+++ ETAPA2/2-Mamani-Torres-2/2-Mamani-Torres-2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Mamani_Torres_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool pasaexamen = false;
+            bool pasatps = false;
+
+            Console.WriteLine(" ¿ Cuantos TPs tienen ?");
+            int TPS = int.Parse(Console.ReadLine());
+            Console.WriteLine(" ¿ Cuantos examanes tienen ?");
+            int examenes = int.Parse(Console.ReadLine());
+
+            int[] notporexamen = new int[examenes];
+            int[] notportps = new int[TPS];
+
+            double totalexamen = 0;
+
+            for (int i = 1; i <= examenes; i++)
+            {
+                Console.WriteLine(" ¿ Cuanto se sacaron en el examen N° " + i );
+                notporexamen[i-1] = int.Parse(Console.ReadLine());
+                totalexamen += notporexamen[i-1];
+
+                double promedio = totalexamen / examenes; 
+                 
+                if (promedio >= 6)
+                {
+                    pasaexamen = true;
+                }
+                else
+                {
+                    pasaexamen = false;
+                }
+            }
+
+            double totaltps = 0;
+            for (int i = 1; i <= TPS; i++)
+            {
+                Console.WriteLine(" ¿ Cuanto se sacaron en el tp N° " + i);
+                notportps[i - 1] = int.Parse(Console.ReadLine());
+
+                totaltps += notportps[i-1];
+
+                double porcentaje =  totaltps * 0.75;                  
+                if (porcentaje >= 6)
+                {
+                    pasatps = true;
+
+                }   
+            }
+            
+
+            if (pasaexamen == true && pasatps == true)
+
+            {
+                Console.WriteLine(" Fhinias y Ferb Pasan ");
+            }
+            else
+            {
+                Console.WriteLine("Fhinias y Ferb No Pasan ");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
